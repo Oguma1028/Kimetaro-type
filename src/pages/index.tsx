a3c3e372f8b9f43710c0ff968aaf387c0b9fa694
Oguma1028 <neetoguma@gmail.com>
2023-02-14 22:28:39 +0900

    Topページのとりあえず中央寄せとリンクだけ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
 import type { NextPage } from 'next'
-import { Box, Heading } from '@chakra-ui/react'
+import { Box, Button, Container, VStack } from '@chakra-ui/react'
+import Link from 'next/link'
 
 const Page: NextPage = () => {
   return (
-    <Box>
-      <Heading>LPが来るよ</Heading>
-    </Box>
+    <Container>
+      <Box>
+        <VStack spacing={4}>
+          <Link href="signin">
+            <Button type={'submit'} colorScheme="red">
+              新規登録
+            </Button>
+          </Link>
+          <Link href="signup">
+            <Button>ログイン</Button>
+          </Link>
+          <Link href="/">
+            <Button>登録せず始める</Button>
+          </Link>
+        </VStack>
+      </Box>
+    </Container>
   )
 }
 
